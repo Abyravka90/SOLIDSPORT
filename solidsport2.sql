7627a63d5405bc418570221072c134da264c4faf
Abyravka90 <asepcahya.nugraha75@gmail.com>
2021-01-20 15:31:34 +0700

    uploading latest database

--- solidsport2.sql
+++ solidsport2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Des 2020 pada 18.19
+-- Waktu pembuatan: 20 Jan 2021 pada 09.30
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.11
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `solidsport`
+-- Database: `solidsport2`
 --
 
 -- --------------------------------------------------------
@@ -35,18 +35,19 @@ CREATE TABLE `atlet` (
   `namaKata` varchar(50) NOT NULL,
   `grup` varchar(10) NOT NULL,
   `atribut` char(3) NOT NULL,
-  `bermain` int(1) NOT NULL DEFAULT 0
+  `bermain` int(1) NOT NULL DEFAULT 0,
+  `statusPenilaian` varchar(10) NOT NULL DEFAULT 'standby'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `atlet`
 --
 
-INSERT INTO `atlet` (`idAtlet`, `namaAtlet`, `kelas`, `kontingen`, `namaKata`, `grup`, `atribut`, `bermain`) VALUES
-(1, 'Anji Nur Gilang', 'Kadet Putra', 'Citra Indah', 'Gojushiho', 'B', 'Ao', 0),
-(2, 'Raya Tegas Syuhada', 'Kadet Putra', 'Hiroshima', 'Unsu', 'B', 'Ao', 0),
-(3, 'Asep Cahya Nugraha', 'Kadet Putra', 'Gojukai Chaki', 'Popuren', 'B1', 'Ao', 0),
-(4, 'Ade Ajie Ferizal', 'Kadet Putra', 'INKAI Jakarta', 'Suparimpei', 'B1', 'Ao', 0);
+INSERT INTO `atlet` (`idAtlet`, `namaAtlet`, `kelas`, `kontingen`, `namaKata`, `grup`, `atribut`, `bermain`, `statusPenilaian`) VALUES
+(1, 'Anji Nur Gilang', 'Kadet Putra', 'Citra Indah', 'Gojushiho', 'B', 'Ao', 1, 'standby'),
+(2, 'Raya Tegas Syuhada', 'Kadet Putra', 'Hiroshima', 'Unsu', 'B', 'Ao', 1, 'standby'),
+(3, 'Asep Cahya Nugraha', 'Kadet Putra', 'Gojukai Chaki', 'Popuren', 'B1', 'Ao', 0, 'standby'),
+(4, 'Ade Ajie Ferizal', 'Kadet Putra', 'INKAI Jakarta', 'Suparimpei', 'B1', 'Ao', 0, 'standby');
 
 -- --------------------------------------------------------
 
@@ -56,12 +57,20 @@ INSERT INTO `atlet` (`idAtlet`, `namaAtlet`, `kelas`, `kontingen`, `namaKata`, `
 
 CREATE TABLE `klasemen` (
   `idKlasemen` int(11) NOT NULL,
-  `idAtlet` int(11) NOT NULL,
+  `idAtlet` char(3) NOT NULL,
   `namaAtlet` varchar(50) NOT NULL,
   `kontingen` varchar(50) NOT NULL,
   `grup` varchar(50) NOT NULL,
   `totalPoint` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `klasemen`
+--
+
+INSERT INTO `klasemen` (`idKlasemen`, `idAtlet`, `namaAtlet`, `kontingen`, `grup`, `totalPoint`) VALUES
+(1, '1', 'Anji Nur Gilang', 'Citra Indah', 'B', 21.26),
+(2, '2', 'Raya Tegas Syuhada', 'Hiroshima', 'B', 22.02);
 
 -- --------------------------------------------------------
 
@@ -70,16 +79,18 @@ CREATE TABLE `klasemen` (
 --
 
 CREATE TABLE `papanskor` (
-  `status` varchar(10) NOT NULL
+  `jenisScoreboard` varchar(10) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `grup` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `papanskor`
 --
 
-INSERT INTO `papanskor` (`status`) VALUES
-('klasemen'),
-('scoreboard');
+INSERT INTO `papanskor` (`jenisScoreboard`, `status`, `grup`) VALUES
+('klasemen', 'idle', ''),
+('scoreboard', 'aktif', '-');
 
 -- --------------------------------------------------------
 
@@ -89,6 +100,7 @@ INSERT INTO `papanskor` (`status`) VALUES
 
 CREATE TABLE `point` (
   `idPoint` int(11) NOT NULL,
+  `idAtlet` char(3) NOT NULL,
   `namaAtlet` varchar(50) NOT NULL,
   `kelas` varchar(50) NOT NULL,
   `kontingen` varchar(50) NOT NULL,
@@ -97,19 +109,21 @@ CREATE TABLE `point` (
   `atribut` char(3) NOT NULL,
   `namaJuri` varchar(50) NOT NULL,
   `nilaiTeknik` double NOT NULL,
-  `nilaiAtletik` double NOT NULL
+  `nilaiAtletik` double NOT NULL,
+  `statusPenilaian` varchar(10) NOT NULL DEFAULT 'standby',
+  `juriMenilai` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `point`
 --
 
-INSERT INTO `point` (`idPoint`, `namaAtlet`, `kelas`, `kontingen`, `namaKata`, `grup`, `atribut`, `namaJuri`, `nilaiTeknik`, `nilaiAtletik`) VALUES
-(1, '-', '-', '-', '-', '-', '-', 'J-1', 0, 0),
-(2, '-', '-', '-', '-', '-', '-', 'J-2', 0, 0),
-(3, '-', '-', '-', '-', '-', '-', 'J-3', 0, 0),
-(4, '-', '-', '-', '-', '-', '-', 'J-4', 0, 0),
-(5, '-', '-', '-', '-', '-', '-', 'J-5', 0, 0);
+INSERT INTO `point` (`idPoint`, `idAtlet`, `namaAtlet`, `kelas`, `kontingen`, `namaKata`, `grup`, `atribut`, `namaJuri`, `nilaiTeknik`, `nilaiAtletik`, `statusPenilaian`, `juriMenilai`) VALUES
+(1, '2', 'Raya Tegas Syuhada', 'Kadet Putra', 'Hiroshima', 'Unsu', 'B', 'Ao', 'J-1', 7.4, 7.2, 'saved', 1),
+(2, '2', 'Raya Tegas Syuhada', 'Kadet Putra', 'Hiroshima', 'Unsu', 'B', 'Ao', 'J-2', 7.4, 7.2, 'saved', 1),
+(3, '2', 'Raya Tegas Syuhada', 'Kadet Putra', 'Hiroshima', 'Unsu', 'B', 'Ao', 'J-3', 7.4, 7.2, 'saved', 1),
+(4, '2', 'Raya Tegas Syuhada', 'Kadet Putra', 'Hiroshima', 'Unsu', 'B', 'Ao', 'J-4', 7.4, 7.2, 'saved', 1),
+(5, '2', 'Raya Tegas Syuhada', 'Kadet Putra', 'Hiroshima', 'Unsu', 'B', 'Ao', 'J-5', 7.4, 7.2, 'saved', 1);
 
 -- --------------------------------------------------------
 
@@ -152,7 +166,7 @@ ALTER TABLE `klasemen`
 -- Indeks untuk tabel `papanskor`
 --
 ALTER TABLE `papanskor`
-  ADD PRIMARY KEY (`status`);
+  ADD PRIMARY KEY (`jenisScoreboard`);
 
 --
 -- Indeks untuk tabel `point`
@@ -180,13 +194,13 @@ ALTER TABLE `atlet`
 -- AUTO_INCREMENT untuk tabel `klasemen`
 --
 ALTER TABLE `klasemen`
-  MODIFY `idKlasemen` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idKlasemen` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `point`
 --
 ALTER TABLE `point`
-  MODIFY `idPoint` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idPoint` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
