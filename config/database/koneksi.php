ade8a3a27063c451f1e2fa98e21455a1fdd61925
maslow123 <mfadhlyn@gmail.com>
2021-02-05 20:08:43 +0700

    Adjust connection DB to hosting

--- config/database/koneksi.php
+++ config/database/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $dbName = "solidsport2";
+    $server = "meera.id.rapidplex.com";
+    $username = "bisapast_solidsport";
+    $password = "solidsport123";
+    $dbName = "bisapast_asa_farma";
 
     $conn = mysqli_connect($server, $username, $password, $dbName);
     if(mysqli_connect_errno()){ echo 'Failed to connect to Mysql : '.mysqli_connect_error();exit();}
