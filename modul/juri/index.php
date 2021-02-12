4ece8d1302ef617990b70b6e7724ce8c8a3856f3
luull <luull@luulls-MacBook-Pro.local>
2021-02-12 07:55:16 +0700

    issue font in list klasement

--- modul/juri/index.php
+++ modul/juri/index.php
@@ -1,50 +1,4 @@
 <?php
-<<<<<<< HEAD
-    session_start();
-    if (!isset($_SESSION['username'])){
-        header("location:../login");
-    } else {
-      if($_SESSION['statusLogin'] == 0){
-      include "../../config/templates/header.php";
-      ?>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Start your development with a Dashboard for Bootstrap 4.">
-    <meta name="author">
-    <title>.:SOLIDSPORTS:.</title>
-    <!-- Favicon -->
-    <link rel="icon" href="assets/img/brand/favicon.png" type="image/png">
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
-    <!-- Icons -->
-  </head>
-  <body background="../../assets/img/banner2.jpg" class="g-sidenav-show g-sidenav-pinned" style="min-height: 100vh;">
-    <!-- Navbar -->
-    
-    <div class="main-content">
-      <!-- Header -->
-      <div class="header py-6 py-lg-7 pt-lg-7">    
-      </div>
-      <!-- Page content -->
-      <div class="container mt--8 pb-5">
-        <div class="row justify-content-center">
-          <div class="col-lg-5 col-md-7">
-            <div class="card  border-0 mb-0">
-              <div class="card-body px-lg-5 py-lg-5">
-                    <h1 class="display-3 text-center"><i class="ni ni-paper-diploma"></i> Penilaian <?= $_SESSION['username'] ?></h1>
-                    <?php $username = $_SESSION['username'] ?>  
-                    <form role="form" method="post" action="prosesPenilaian.php">
-                        <div class="text-center">
-                          <input type="hidden" name="username" value="<?= $username ?>">
-                          <button type="submit" class="btn btn-primary my-4">proses</button>
-                          <br>
-                      </form>
-                        <img height="75px" src="../../assets/img/logo-2.jpeg">
-                      </div>
-                     
-=======
 session_start();
 if (!isset($_SESSION['username'])) {
   header("location:../login");
@@ -95,7 +49,6 @@ if (!isset($_SESSION['username'])) {
                       <a class="btn btn-light btn-sm mt-3 mb-4" style="float: left;" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solidsport/modul/logout"> <i class="fas fa-angle-left"></i> Keluar</a>
                   </form>
                 </div>
->>>>>>> c8b2a11534bd7e12e2710c2e14dce395784a56e4
               </div>
             </div>
           </div>
@@ -104,21 +57,6 @@ if (!isset($_SESSION['username'])) {
       </div>
 
 
-<<<<<<< HEAD
-    <center><a style="margin-top:400px;" class="btn btn-danger" href="http://<?= $_SERVER['HTTP_HOST'] ?>/solidsport/modul/logout">logout</a></center>
-    <!-- Argon Scripts -->
-    <!-- Core -->
-    <script src="../../assets/vendor/jquery/dist/jquery.min.js"></script>
-   <script src="../../assets/vendor/js-cookie/js.cookie.js"></script>
-    <script src="../../assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
-    <!-- Argon JS -->
-    <script src="../../assets/js/argon.js?v=1.2.0"></script><div class="backdrop d-xl-none" data-action="sidenav-unpin" data-target="undefined"></div>
-  </body></html>
-  <?php 
-        } else {
-                echo 
-                '<script>
-=======
       <!-- Argon Scripts -->
       <!-- Core -->
       <script src="../../assets/vendor/jquery/dist/jquery.min.js"></script>
@@ -134,7 +72,6 @@ if (!isset($_SESSION['username'])) {
   } else {
     echo
     '<script>
->>>>>>> c8b2a11534bd7e12e2710c2e14dce395784a56e4
                 alert("reset login melalui admin");
                 window.location.href="../login";
                 </script>';
