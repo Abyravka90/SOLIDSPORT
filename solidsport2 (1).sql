81795372720caad6e7a553ef46d1fca32342c177
Abyravka90 <asepcahya.nugraha75@gmail.com>
2021-02-12 10:56:25 +0700

    new database

--- solidsport2 (1).sql
+++ solidsport2 (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Feb 2021 pada 03.11
+-- Waktu pembuatan: 12 Feb 2021 pada 04.55
 -- Versi server: 10.4.14-MariaDB
 -- Versi PHP: 7.4.11
 
@@ -44,31 +44,14 @@ CREATE TABLE `atlet` (
 --
 
 INSERT INTO `atlet` (`idAtlet`, `namaAtlet`, `kelas`, `kontingen`, `namaKata`, `grup`, `atribut`, `bermain`, `statusPenilaian`) VALUES
-(1, 'ALIF LANGIT RAMADHAN ', 'USIA DINI PUTRA', ' PREDATOR KARATE CLUB', 'HEIAN YONDAN ', 'A1', 'Ao', 0, 'standby'),
+(1, 'ALIF LANGIT RAMADHAN ', 'USIA DINI PUTRA', ' PREDATOR KARATE CLUB', 'HEIAN YONDAN ', 'A1', 'Ao', 1, 'standby'),
 (2, 'NIZAM FAIZ', 'USIA DINI PUTRA', 'DOJO OPUNG ASKI', 'HEIAN YONDAN', 'A1', 'Ao', 0, 'standby'),
 (3, 'Wahyu Williansah', 'USIA DINI PUTRA', 'BENGPUSHUB PUSHUBAD KOTA BANDUNG', 'HEIAN YONDAN', 'A1', 'Ao', 0, 'standby'),
 (4, 'Daniel Florencio Dermawan', 'USIA DINI PUTRA', 'SDN RRI CISALAK', 'HEIAN YONDAN', 'A1', 'Ao', 0, 'standby'),
 (5, 'ABDILLATUL QUBRO', 'USIA DINI PUTRA', 'AMC INKANAS DKI JAKARTA', 'HEIAN YONDAN', 'A1', 'Ao', 0, 'standby'),
 (6, 'TRAVIS NATHA DAFFA KUMARA', 'USIA DINI PUTRA', 'DONSKA', 'HEIAN YONDAN', 'A1', 'Ao', 0, 'standby'),
 (7, 'YUSUF AR RAHMAN RUSLI', 'USIA DINI PUTRA', 'COAST GUARD KARATE TEAM - SULAWESI TENGGARA', 'HEIAN YONDAN', 'A2', 'Ao', 0, 'standby'),
-(8, 'HAFIZH MAULANA', 'USIA DINI PUTRA', 'GOJU ASS LAMPUNG', 'Gekisai Dai Ich', 'A2', 'Ao', 0, 'standby'),
-(9, 'Mochamad  Raihan NN', 'USIA DINI PUTRA', 'CIPAISAN PURWAKARTA', 'HEIAN SODAN', 'A2', 'Ao', 0, 'standby'),
-(10, 'HIROKI AMMARKIANDRA PITANA', 'USIA DINI PUTRA', 'AGGAZI KARATE CLUB', 'HEIAN YONDAN', 'A2', 'Ao', 0, 'standby'),
-(11, 'MUHAMMAD SAFARAZ AKMA WAFRIE', 'USIA DINI PUTRA', 'UNTAN KARATE CLUB', 'HEIAN YONDAN', 'A2', 'Ao', 0, 'standby'),
-(12, 'MUHAMMAD ALIEF PUTRA SATRIA', 'USIA DINI PUTRA', 'DONSKA', 'HEIAN YONDAN', 'A2', 'Ao', 0, 'standby'),
-(13, 'ALFATH RIZQY NASRUDDIN ', 'USIA DINI PUTRA', ' PREDATOR KARATE CLUB', 'HEIAN YONDAN ', 'B1', 'Ao', 0, 'standby'),
-(14, 'M. FAUZAN AL-BARIZY', 'USIA DINI PUTRA', 'GOJU ASS LAMPUNG', 'Gekisai Dai Ich', 'B1', 'Ao', 0, 'standby'),
-(15, 'ALFARIZI', 'USIA DINI PUTRA', 'DOJO OPUNG ASKI', 'HEIAN YONDAN', 'B1', 'Ao', 0, 'standby'),
-(16, 'Bintang Aleka Pratama Yusa', 'USIA DINI PUTRA', 'KOREM 064 MAULANA YUSUF', 'HEIAN YONDAN', 'B1', 'Ao', 0, 'standby'),
-(17, 'M. Akbar Alfatih M', 'USIA DINI PUTRA', 'SDN RRI CISALAK', 'HEIAN YONDAN ', 'B1', 'Ao', 0, 'standby'),
-(18, 'ZESHAN EXEL RIZKY SATRIA RAMADHAN', 'USIA DINI PUTRA', 'AMC INKANAS DKI JAKARTA', 'HEIAN YONDAN', 'B1', 'Ao', 0, 'standby'),
-(19, 'RAYYAN AGHA MURDANA', 'USIA DINI PUTRA', 'DONSKA', 'HEIAN YONDAN ', 'B1', 'Ao', 0, 'standby'),
-(20, 'MUHAMMAD KAUTSAR MEHAGA PERANGINANGIN', 'USIA DINI PUTRA', 'BUMI MAKMUR', 'HEIAN YONDAN', 'B2', 'Ao', 0, 'standby'),
-(21, 'M. ASKAR GOJURYU', 'USIA DINI PUTRA', 'GOJU ASS LAMPUNG', 'Gekisai Dai Ich', 'B2', 'Ao', 0, 'standby'),
-(22, 'Virzatollah Ramadhan hillary', 'USIA DINI PUTRA', 'CIPAISAN PURWAKARTA', 'HEIAN YONDAN', 'B2', 'Ao', 0, 'standby'),
-(23, 'M. AKBAR RAFSANJANI', 'USIA DINI PUTRA', 'PUMA KARATE CLUB PONTIANAK TIMUR KALBAR', 'HEIAN YONDAN', 'B2', 'Ao', 0, 'standby'),
-(24, 'AHMAD MADINAH', 'USIA DINI PUTRA', 'DONSKA', 'HEIAN YONDAN', 'B2', 'Ao', 0, 'standby'),
-(25, 'MUHAMMAD YAZID ILMANY WIJAYA', 'USIA DINI PUTRA', 'DONSKA', 'HEIAN YONDAN', 'B2', 'Ao', 0, 'standby');
+(8, 'HAFIZH MAULANA', 'USIA DINI PUTRA', 'GOJU ASS LAMPUNG', 'Gekisai Dai Ich', 'A2', 'Ao', 0, 'standby');
 
 -- --------------------------------------------------------
 
@@ -104,7 +87,7 @@ CREATE TABLE `papanskor` (
 --
 
 INSERT INTO `papanskor` (`jenisScoreboard`, `status`, `kelas`, `grup`) VALUES
-('klasemen', 'idle', '', 'final'),
+('klasemen', 'idle', '', ''),
 ('scoreboard', 'aktif', '', '-');
 
 -- --------------------------------------------------------
@@ -126,19 +109,44 @@ CREATE TABLE `point` (
   `nilaiTeknik` double NOT NULL,
   `nilaiAtletik` double NOT NULL,
   `statusPenilaian` varchar(10) NOT NULL DEFAULT 'standby',
-  `juriMenilai` int(11) NOT NULL DEFAULT 0
+  `juriMenilai` int(11) NOT NULL DEFAULT 0,
+  `bermain` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `point`
 --
 
-INSERT INTO `point` (`idPoint`, `idAtlet`, `namaAtlet`, `kelas`, `kontingen`, `namaKata`, `grup`, `atribut`, `namaJuri`, `nilaiTeknik`, `nilaiAtletik`, `statusPenilaian`, `juriMenilai`) VALUES
-(1, '-', '-', '-', '-', '-', '-', '-', 'J-1', 0, 0, 'standby', 0),
-(2, '-', '-', '-', '-', '-', '-', '-', 'J-2', 0, 0, 'standby', 0),
-(3, '-', '-', '-', '-', '-', '-', '-', 'J-3', 0, 0, 'standby', 0),
-(4, '-', '-', '-', '-', '-', '-', '-', 'J-4', 0, 0, 'standby', 0),
-(5, '-', '-', '-', '-', '-', '-', '-', 'J-5', 0, 0, 'standby', 0);
+INSERT INTO `point` (`idPoint`, `idAtlet`, `namaAtlet`, `kelas`, `kontingen`, `namaKata`, `grup`, `atribut`, `namaJuri`, `nilaiTeknik`, `nilaiAtletik`, `statusPenilaian`, `juriMenilai`, `bermain`) VALUES
+(1, '-', '-', '-', '-', '-', '-', '-', 'J-1', 0, 0, 'standby', 0, 0),
+(2, '-', '-', '-', '-', '-', '-', '-', 'J-2', 0, 0, 'standby', 0, 0),
+(3, '-', '-', '-', '-', '-', '-', '-', 'J-3', 0, 0, 'standby', 0, 0),
+(4, '-', '-', '-', '-', '-', '-', '-', 'J-4', 0, 0, 'standby', 0, 0),
+(5, '-', '-', '-', '-', '-', '-', '-', 'J-5', 0, 0, 'standby', 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `rekap`
+--
+
+CREATE TABLE `rekap` (
+  `idRekap` int(11) NOT NULL,
+  `idAtlet` int(11) NOT NULL,
+  `grup` varchar(10) NOT NULL,
+  `T1` double NOT NULL,
+  `T2` double NOT NULL,
+  `T3` double NOT NULL,
+  `T4` double NOT NULL,
+  `T5` double NOT NULL,
+  `A1` double NOT NULL,
+  `A2` double NOT NULL,
+  `A3` double NOT NULL,
+  `A4` double NOT NULL,
+  `A5` double NOT NULL,
+  `bermain` int(11) NOT NULL,
+  `totalPoint` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -195,6 +203,12 @@ ALTER TABLE `point`
   ADD PRIMARY KEY (`idPoint`);
 
 --
+-- Indeks untuk tabel `rekap`
+--
+ALTER TABLE `rekap`
+  ADD PRIMARY KEY (`idRekap`);
+
+--
 -- Indeks untuk tabel `user`
 --
 ALTER TABLE `user`
@@ -208,7 +222,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `atlet`
 --
 ALTER TABLE `atlet`
-  MODIFY `idAtlet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `idAtlet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `klasemen`
@@ -221,6 +235,12 @@ ALTER TABLE `klasemen`
 --
 ALTER TABLE `point`
   MODIFY `idPoint` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `rekap`
+--
+ALTER TABLE `rekap`
+  MODIFY `idRekap` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
